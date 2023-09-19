cf64476d5db618e0b6cb4805fad226bb85b44928
akash0651 <55975966+akash0651@users.noreply.github.com>
2023-09-19 15:35:41 +0530

    Update review.js

--- models/review.js
+++ models/review.js
@@ -1,6 +1,6 @@
-
 const mongoose = require('mongoose');
 
+// Define the schema for a review
 const reviewSchema = new mongoose.Schema({
 
     review : {
@@ -9,18 +9,18 @@ const reviewSchema = new mongoose.Schema({
     },
     from : {
         type : mongoose.Schema.Types.ObjectId,
-        ref : 'User',
+        ref : 'User', // Reference to the User model for the sender of the review
     },
     to : {  
         type : mongoose.Schema.Types.ObjectId,
-        ref : 'User',
+        ref : 'User', // Reference to the User model for the recipient of the review
     }
 
 },{
-    timestamps : true,
+    timestamps : true, // Adds timestamps for when the review was created and updated
 });
 
-
+// Create a model for the review schema
 const Review = mongoose.model('Review', reviewSchema);
 
-module.exports = Review;
+module.exports = Review; // Export the Review model

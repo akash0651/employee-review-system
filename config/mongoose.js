c6f0908edb1ea16efe96659c8a0ce57a5e84f85c
akash0651 <55975966+akash0651@users.noreply.github.com>
2023-09-19 15:29:56 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
 
+// Set mongoose option to allow undefined fields in queries
 mongoose.set('strictQuery', false);
+
+// Connect to MongoDB using the provided URL
 mongoose.connect("mongodb+srv://kumarakashsahu98765:hJuBI3O9Vu9EWma7@cluster0.7jpcgdb.mongodb.net/ers-db");
 
+// Get the database connection
+const db = mongoose.connection;
 
-const db= mongoose.connection;
-db.on("error", console.error.bind(console,"Error in connection to Mongodb"));
+// Event listener for connection error
+db.on("error", console.error.bind(console, "Error in connection to MongoDB"));
 
-
+// Event listener for successful connection
 db.once("open", function(){
     console.log("Successfully connected to Database :: MongoDB");
-
-    
 });
 
-module.exports =db;
-
+// Export the database connection
+module.exports = db;

36d6897ab78f50a1fb339810b2468cf8d062e492
akash0651 <55975966+akash0651@users.noreply.github.com>
2023-09-19 15:37:12 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
-
 const mongoose = require('mongoose');
 
+// Define the schema for a user
 const userSchema = new mongoose.Schema({
 
     name : {
@@ -22,21 +22,21 @@ const userSchema = new mongoose.Schema({
     to : [     
         {
             type : mongoose.Schema.Types.ObjectId,
-            ref : 'User',
+            ref : 'User', // Reference to other User documents, indicating to whom this user sends reviews
         }
     ],
     from : [    
         {
             type : mongoose.Schema.Types.ObjectId,
-            ref : 'Review',
+            ref : 'Review', // Reference to Review documents, indicating reviews received by this user
         }
     ]
 
 },{
-    timestamps : true,
+    timestamps : true, // Adds timestamps for when the user document was created and updated
 });
 
-
+// Create a model for the user schema
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = User; // Export the User model
